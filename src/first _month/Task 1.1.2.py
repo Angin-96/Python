d0d33989635803c8bcadfa9c35ac93234cdbf1ff
Angin-96 <angin.mgrdunc@mail.ru>
2021-04-18 13:41:26 +0400

    Changed

--- src/first _month/Task 1.1.2.py
+++ src/first _month/Task 1.1.2.py
@@ -3,58 +3,55 @@
 # 1. Write a Python program to get a string made of the first 2 and the last 2 chars from a given string.
 c='asdfghjk'
 s=c[:2]+c[-2:]
-print("s=", c[:2]+c[-2:])
-s= asjk
+print("s=", s)
+
 
 
 # 2. Write a Python program to remove the n-th index character from a nonempty string.
 d='qwertrir'
-f=d[:6]
-print("f=", d[:6])
-f= qwertr
-print("f=", d[:7])
-f= qwertri
+n=4
+c=d[0:n]+d[n+1:]
+print(c)
 
 
 
 # 3. Write a Python program to change a given string to a new string where the first and last chars have been exchanged.
 f='asdfg'
-l='d'+f[1:4]+'f'
-print("l=", 'd'+f[1:4]+'f')
-l= dsdff
+l=f[-1]+f[1:-1]+f[0]
+print("l=", l)
 
 
 
 # 4. Write a Python function to get a string made of 4 copies of the last two characters of a specified string.
 df='123654'
 gh=df[-2:]*4
-print("gh=", df[-2:]*4)
-gh= 54545454
+print("gh=", gh)
+
 
 
 
 # Lists
 # 1. Write a Python program that make a list from given string.
 bravo='bravocharlie'
-foxtrot=[bravo]
-print("foxtrot=", [bravo])
-foxtrot= ['bravocharlie']
+foxtrot=list(bravo)
+print(foxtrot)
+
 
 
 
 # 2. Write a Python program to print a new list which is the equivalent given list after removing the 0th, 4th and 5th elements.
-sierra=['tango', ['yanki',565], 'rojer', 5456, 'hd', '68687']
-echo=sierra[1:4]
-print("echo=", sierra[1:4])
-echo= [['yanki', 565], 'rojer', 5456]
+sierra=['tango', ['yanki',565], 'rojer', 5456, 'hd', '68687',69,'nhfds']
+echo=sierra[1:4]+sierra[6:]
+print(echo)
+
 
 
 
 # 3. Write a Python program which add an element to the given list.
 alfa=[1,2,3,'gh',5]
 alfa=alfa+['h',122,'havana']
-print("alfa=", alfa+['h',122,'havana'])
-alfa= [1, 2, 3, 'gh', 5, 'h', 122, 'havana', 'h', 122, 'havana']
+print("alfa=", alfa)
+
 
 
 
@@ -62,6 +59,6 @@ alfa= [1, 2, 3, 'gh', 5, 'h', 122, 'havana', 'h', 122, 'havana']
 cyber=[1,2,3, 'crazy']
 security=['hak',122,'hj',['jjkf',155]]
 protection=cyber+security
-print("protection=", cyber+security)
-protection= [1, 2, 3, 'crazy', 'hak', 122, 'hj', ['jjkf', 155]]
+print("protection=",protection)
+
 
--- src/first _month/task 1.2.1.py
+++ src/first _month/task 1.2.1.py
@@ -1,13 +1,16 @@
 #Write a Python program to get the largest number from a list.
 s=[1,56,8,96,5,86,23,74,6,8]
 s.sort()
-print(s[-1])
+f=s[-1]
+print(f)
 
 
 
 #Write a Python program to get the frequency of the given element in a list to.
 s=[1,2,3,5,5,6,7,8,5]
-s.count(5)
+d=s.count(5)
+print(d)
+
 
 
 
@@ -15,8 +18,8 @@ s.count(5)
 s=[1,2,3,4,1,5,6,1]
 a=s.index(1,2)
 print(a)
-s.pop(a)
-print(s)
+d=s.pop(a)
+print(d)
 
 
 
